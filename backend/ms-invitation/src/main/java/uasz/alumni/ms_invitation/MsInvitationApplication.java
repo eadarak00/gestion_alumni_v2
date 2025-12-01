317e7327b1cc055d1adcf9125ec784f1d53db06b
eadarak00 <eadarak2001@gmail.com>
2025-12-01 15:26:40 +0000

    feat: Initialize the ms-invitation Spring Boot microservice with basic setup and Maven Wrapper.

---
+++ backend/ms-invitation/src/main/java/uasz/alumni/ms_invitation/MsInvitationApplication.java
@@ -0,0 +1,13 @@
+package uasz.alumni.ms_invitation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsInvitationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsInvitationApplication.class, args);
+	}
+
+}
