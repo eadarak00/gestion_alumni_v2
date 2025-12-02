2c9c00cd7bf115359a6a4f97014fd6a4c931959a
eadarak00 <eadarak2001@gmail.com>
2025-12-02 00:49:21 +0000

    feat: initial setup of ms-cv microservice with Spring Boot and Maven wrapper

---
+++ backend/ms-cv/src/main/java/uasz/alumni/ms_cv/MsCvApplication.java
@@ -0,0 +1,13 @@
+package uasz.alumni.ms_cv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsCvApplication.class, args);
+	}
+
+}
