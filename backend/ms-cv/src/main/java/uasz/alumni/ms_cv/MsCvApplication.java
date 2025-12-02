d738f698d04c587f4c14cd512a5618857e5a1584
eadarak00 <eadarak2001@gmail.com>
2025-12-02 00:57:28 +0000

    feat: Add ms-cv service and its database to Docker Compose, and enable Eureka discovery for ms-cv and ms-invitation.

--- backend/ms-cv/src/main/java/uasz/alumni/ms_cv/MsCvApplication.java
+++ backend/ms-cv/src/main/java/uasz/alumni/ms_cv/MsCvApplication.java
@@ -2,8 +2,10 @@ package uasz.alumni.ms_cv;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MsCvApplication {
 
 	public static void main(String[] args) {
--- backend/ms-invitation/src/main/java/uasz/alumni/ms_invitation/MsInvitationApplication.java
+++ backend/ms-invitation/src/main/java/uasz/alumni/ms_invitation/MsInvitationApplication.java
@@ -2,8 +2,10 @@ package uasz.alumni.ms_invitation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MsInvitationApplication {
 
 	public static void main(String[] args) {
