d6b2a0f02a34dbaabab7bd0c834216d79998c046
eadarak00 <eadarak2001@gmail.com>
2025-12-02 23:00:11 +0000

    feat: Enable Spring Security auto-configuration by removing exclusion

--- backend/ms-user/src/main/java/uasz/alumni/ms_user/MsUserApplication.java
+++ backend/ms-user/src/main/java/uasz/alumni/ms_user/MsUserApplication.java
@@ -3,14 +3,13 @@ package uasz.alumni.ms_user;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
-@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@SpringBootApplication
 @EnableDiscoveryClient
 @EnableJpaAuditing
 @RequiredArgsConstructor
