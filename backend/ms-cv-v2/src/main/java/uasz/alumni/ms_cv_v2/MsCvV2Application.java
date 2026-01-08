c06f4dce9d56e7a1a4319fecc48d6f3bc729bfd4
eadarak00 <eadarak2001@gmail.com>
2026-01-08 18:37:20 +0000

    init projet

---
+++ backend/ms-cv-v2/src/main/java/uasz/alumni/ms_cv_v2/MsCvV2Application.java
@@ -0,0 +1,13 @@
+package uasz.alumni.ms_cv_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCvV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsCvV2Application.class, args);
+	}
+
+}
---
+++ backend/ms-cv-v2/src/test/java/uasz/alumni/ms_cv_v2/MsCvV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package uasz.alumni.ms_cv_v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsCvV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
